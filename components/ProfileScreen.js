45423891bd6735646871fcf95d12de33aa329ae7
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2020-10-30 13:56:46 +0100

    Added a profile avatar

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { BigHead } from 'react-native-bigheads'
 
-export default function ListScreen({ navigation }) {
+export default function ProfileScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
+      <BigHead
+  accessory="none"
+  bgColor="blue"
+  bgShape="circle"
+  body="chest"
+  clothing="shirt"
+  clothingColor="black"
+  eyebrows="raised"
+  eyes="normal"
+  facialHair="none"
+  graphic="none"
+  hair="short"
+  hairColor="black"
+  hat="none"
+  lashes={false}
+  lipColor="red"
+  mouth="grin"
+  showBackground={true}
+  size={200}
+  skinTone="brown"
+/>
+      <Text>Name:</Text>
+      <Text>Name:</Text>
+      <Text>Name:</Text>
       <Button
         title="Go back to list"
         onPress={() => navigation.navigate('List')}
