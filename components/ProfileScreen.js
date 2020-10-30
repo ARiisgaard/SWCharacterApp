064fa974ed59e8cf7775703c3a7e246ad355d6cf
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2020-10-30 19:32:11 +0100

    Data about character is now based on fetch request

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import Avatar from './Avatar.js';
 
 export default function ProfileScreen({ navigation }) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://swapi.dev/api/people/1/',
+      );
+
+      setData(result.data);
+      console.log(result.data)
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Avatar
@@ -11,9 +27,10 @@ export default function ProfileScreen({ navigation }) {
     hairColor={"black"}
     size={300}
     />
-      <Text>Name:</Text>
-      <Text>Name:</Text>
-      <Text>Name:</Text>
+      <Text>Name: {data.name}</Text>
+      <Text>Height: {data.height}</Text>
+      <Text>Weight: {data.mass}</Text>
+      <Text>Weight: {data.birth_year}</Text>
       <Button
         title="Go back to list"
         onPress={() => navigation.navigate('List')}
