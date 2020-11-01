a215a4cafe0ddef739174fedd6f61037ce03e2a6
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2020-11-01 13:11:32 +0100

    List is based on data from the api

--- components/ListScreen.js
+++ components/ListScreen.js
@@ -1,57 +1,34 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View, FlatList, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button, StyleSheet, Text, View, FlatList, StatusBar, ActivityIndicator } from 'react-native';
 import Avatar from './Avatar.js';
 
 export default function ListScreen({ navigation }) {
 
-  const names = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    name: 'Luke Skywalker',
-    planet: 'Earth',
-    gender: "male",
-    skin_color: "black",
-    hair_color: "pink"
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    name: 'C-3PO',
-    planet: 'Earth',
-    gender: "male",
-    skin_color: "black",
-    hair_color: "orange"
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    name: 'R2-D2',
-    planet: 'Mars',
-    gender: "male",
-    skin_color: "black",
-    hair_color: "orange"
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d71',
-    name: 'Darth Vader',
-    planet: 'Mars',
-    gender: "female",
-    skin_color: "black",
-    hair_color: "orange"
-  },  {
-      id: '58694a0f-3da1-471f-bd96-315571e29d71',
-      name: 'Hansolo',
-      planet: 'Mars',
-      gender: "bot",
-      skin_color: "black",
-      hair_color: "orange"
-    },
-];
+  const [people, setPeople] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchPeople = async () => {
+      const result = await axios(
+        'https://swapi.dev/api/people/',
+      );
+
+      setPeople(result.data.results);
+      console.log(result.data.results[2])
+      setLoaded(true);
+    };
+
+    // fetchPlanets();
+    fetchPeople();
+  }, []);
 
 const Item = ({ characterInfo }) => (
   <View style={styles.item}>
   <Avatar
       gender={characterInfo.gender}
-      skinTone={characterInfo.skin_color}
-      hairColor={characterInfo.hair_color}
+      skinTone={characterInfo.skin_color === "fair" ? "light" : "black"}
+      hairColor={characterInfo.hair_color === "blond" ? "blonde" : "pink"}
       size={80}
       />
       <View style={{ width: "100%"}}>
@@ -67,16 +44,22 @@ const renderItem = ({ item }) => (
   <Item characterInfo={item}
   />
 );
+const loadingItem =  <ActivityIndicator
+               color = '#bc2b78'
+               size = "large"/>
+
+
+const loadedList =   <FlatList style={{width: "100%"}}
+    data={people}
+    renderItem={renderItem}
+    keyExtractor={item => item.id}
+  />
 
   return (
     <View style={{ display: "flex", alignItems: 'center', justifyContent: 'center',
     backgroundColor: '#363636'}}>
       <Text style={styles.titleText}>Star Wars Characters</Text>
-  <FlatList style={{width: "100%"}}
-    data={names}
-    renderItem={renderItem}
-    keyExtractor={item => item.id}
-  />
+      {loaded ? loadedList : loadingItem}
     </View>
   );
 }
