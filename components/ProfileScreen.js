745d6eb5c5d5f4f39a9dddc411bba3683ca44bae
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2020-11-02 21:52:04 +0100

    Styled profile

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 import Avatar from './Avatar.js';
 import colorDictionary from './colorDictionary.js';
 
@@ -8,23 +9,61 @@ import colorDictionary from './colorDictionary.js';
 export default function ProfileScreen({ route, navigation }) {
   const { chosenCharacter } = route.params;
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  const CharacterDetails = ({ category,data }) => (
+  <View style={{ flexDirection:'row', width: "100%"}}>
+  <View style={{ alignItems: 'flex-end', width: "50%"}}>
+  <Text style={styles.categoyText}>{category}:  </Text>
+  </View>
 
+  <View style={{ width: "50%"}}>
+  <Text style={styles.infoText}>{data}</Text>
+  </View>
+  </View>
+)
+
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#000000' }}>
+    <Text style={styles.nameText}>{chosenCharacter.name}</Text>
     <Avatar
         gender={chosenCharacter.gender}
         skinTone={typeof colorDictionary[chosenCharacter.skin_color] !== "undefined" ? colorDictionary[chosenCharacter.skin_color] : "yellow"}
         hairColor={typeof colorDictionary[chosenCharacter.hair_color] !== "undefined" ? colorDictionary[chosenCharacter.hair_color] : "pink"}
-        size={300}
+        size={400}
         />
-      <Text>Name: {chosenCharacter.name}</Text>
-      <Text>Height: {chosenCharacter.height}</Text>
-      <Text>Weight: {chosenCharacter.mass}</Text>
-      <Text>Birth Year: {chosenCharacter.birth_year}</Text>
+        <View style={{justifyContent: 'center',}}>
+        <CharacterDetails category={"Height"} data={chosenCharacter.height}/>
+        <CharacterDetails category={"Weight"} data={chosenCharacter.mass}/>
+        <CharacterDetails category={"Birth Year"} data={chosenCharacter.birth_year}/>
+        </View>
       <Button
-        title="Go back to list"
+      buttonStyle={styles.returnButton}
+        title="Return to list"
         onPress={() => navigation.navigate('List')}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  nameText: {
+    fontSize: 30,
+    color: '#f0e43e'
+  },
+  categoyText: {
+    textAlign: "right",
+    fontSize: 20,
+    color: '#f0e43e'
+  },
+  infoText: {
+    textAlign: "left",
+    fontSize: 20,
+    color: '#f0e43e'
+  },
+  returnButton: {
+    backgroundColor: '#363636',
+
+    marginTop: 5,
+  },
+
+});
